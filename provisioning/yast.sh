b4a861e3fba421394f357542742bb80b837b9adb
Imobach González Sosa <igonzalezsosa@suse.com>
2017-05-25 09:53:28 +0100

    Add licenses to Vagrant configuration

--- provisioning/yast.sh
+++ provisioning/yast.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+# Copyright (c) 2017 SUSE LLC.
+#  All Rights Reserved.
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of version 2 or 3 of the GNU General
+#  Public License as published by the Free Software Foundation.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, contact SUSE LLC.
+#
+#  To contact Novell about this file by physical or electronic mail,
+#  you may find current contact information at www.suse.com
+
 # Enable YaST and Ruby development repositories
 zypper ar -f --no-gpgcheck \
      http://download.opensuse.org/repositories/devel:/languages:/ruby:/extensions/openSUSE_Leap_42.1/ \
